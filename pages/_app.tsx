fee78651f47688bfa6c8fc95fbbac92eba879f42
Arrigo Lupori <arrigo.lupori@gmail.com>
2022-03-13 17:35:12 +0100

    CONFIG: Added Fathom analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as Fathom from 'fathom-client'
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	useEffect(() => {
+		// Initialize Fathom when the app loads
+		// Example: yourdomain.com
+		//  - Do not include https://
+		//  - This must be an exact match of your domain.
+		//  - If you're using www. for your domain, make sure you include that here.
+		Fathom.load('PCTAJJBK', {
+			includedDomains: ['www.frontendster.com'],
+		})
+
+		function onRouteChangeComplete() {
+			Fathom.trackPageview()
+		}
+		// Record a pageview when route changes
+		router.events.on('routeChangeComplete', onRouteChangeComplete)
+
+		// Unassign event listener
+		return () => {
+			router.events.off('routeChangeComplete', onRouteChangeComplete)
+		}
+	})
+
+	return <Component {...pageProps} />
 }
 
 export default MyApp
